2dabbec93f22ab34668a1da9b4a66d6527f40adb
Gabriel <costanzag1999@gmail.com>
2022-11-10 09:28:18 +0100

    ..

--- src/main/java/com/crittergestionfx/App.java
+++ src/main/java/com/crittergestionfx/App.java
@@ -2,21 +2,30 @@ package com.crittergestionfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App extends Application {
+
+    private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene = new Scene(loadFXML("critter-index"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
 
     public static void main(String[] args) {
