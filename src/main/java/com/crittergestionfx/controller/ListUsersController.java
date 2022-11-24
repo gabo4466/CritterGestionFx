63ed5d2402a7416e293392704d8bec68dbc5605c
Gabriel <costanzag1999@gmail.com>
2022-11-24 18:09:14 +0100

    Table on list Users

--- src/main/java/com/crittergestionfx/controller/ListUsersController.java
+++ src/main/java/com/crittergestionfx/controller/ListUsersController.java
@@ -1,4 +1,14 @@
 package com.crittergestionfx.controller;
 
+import com.crittergestionfx.model.objects.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class ListUsersController {
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TableView<User> table;
 }
