c64c39aff825f9dec5d6903f93f82a3292d3be37
Gabriel <costanzag1999@gmail.com>
2022-11-28 10:50:45 +0100

    Register interface

--- src/main/java/com/crittergestionfx/controller/RegisterUserController.java
+++ src/main/java/com/crittergestionfx/controller/RegisterUserController.java
@@ -1,11 +1,22 @@
 package com.crittergestionfx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class RegisterUserController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterUserController implements Initializable {
     @FXML
     private Button cancelButton;
 
@@ -35,4 +46,25 @@ public class RegisterUserController {
 
     @FXML
     private TextField repeatPasswordInput;
+
+    private void loadScreen(ActionEvent event, String target) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(target));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.cancelButton.setOnAction(actionEvent -> {
+            try {
+                loadScreen(actionEvent, "main-menu.fxml");
+
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
